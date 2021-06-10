00251b1abc84dc747b33f8940649e9c0c4559989
Julio Campos <85242470+JulioAFC@users.noreply.github.com>
2021-06-10 14:24:24 -0300

    Add files via upload

---
+++ listaCompras10-06.js
@@ -0,0 +1,22 @@
+let listaCompras = ["Arroz","Feijão","Pão","Batata","Carne","Frango","Ovo"];
+//.join()
+//let separadosPorTraco = listaCompras.join("-")
+//console.log(listaCompras); // lista normal
+//console.log (separadosPorTraco); //Arroz-Feijão-Pão-Batata-Carne-Frango-Ovo
+
+//.pop()
+//let listaMenosOvo = listaCompras.pop("Ovo")
+//console.log(listaMenosOvo); // serapa o Ovo e retorna
+
+//.push()
+//let  listaComprasAdd = listaCompras.push("Farinha")
+//console.log(listaComprasAdd);// conta os elementos e restorna o resultado = 8
+
+//.shift
+//let listaComprasMenosPrim = listaCompras.shift("Arroz")
+//console.log(listaComprasMenosPrim); //elimina o 1º índice e o retorna = Arroz
+
+//.unshift()
+//let listaComprasAddMais = listaCompras.unshift("Farinha","Bala")
+//console.log(listaComprasAddMais); // receb 1 ou mais elementos e retorna o novo comprimento do array
+
