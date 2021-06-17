f479b869814d219ce577b2e82cd9fb2e18ffe4a2
Julio Campos <85242470+JulioAFC@users.noreply.github.com>
2021-06-17 07:37:00 -0300

    micro ex-arrow

---
+++ ex-arrow-1.js
@@ -0,0 +1,16 @@
+// fucntion print(mensagem){
+//     console.log(mensagem);
+// }
+// print ("Olá, bom dia")
+//Arrow:
+let print = () => console.log('Olá, bom dia');
+print();
+
+
+// function soma(n1,n2){
+//     retunr n1+n2
+// }
+// console.log(soma(10,10))
+
+let soma = (n1,n2) => n1 + n2;
+console.log (soma(10,10));
